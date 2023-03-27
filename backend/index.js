f7f8c06ee3e4e01cc9a6e8b525aeebd7ff053f5e
sessionkiller <najib.doukkani@gmail.com>
2023-03-27 23:49:10 +0000

    Serve static React App

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('public'))
+
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URL);
